57da8e301f3af36eebc11796b5f12066e295e326
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-04-22 16:58:15 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import ContactList from './contactlist';
 import { nanoid } from 'nanoid';
 import FilterContacts from './filter';
 import Title from './title';
-import { saveDataForm, getDataPrev, saveData} from "./api";
+import {getData, saveData} from "./api"; //saveDataForm
 
 function getRandomHexColor() {
   return `#${Math.floor((0.2+0.5*Math.random()) * 16777215).toString(16)}`;
@@ -23,15 +23,18 @@ const appStyles = {
 };
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
-
+  componentDidMount() { 
+    const contacts = getData();
+    this.setState({contacts});
+  }
+  componentDidUpdate(prevProps, prevState) { 
+    if (prevState.contacts !== this.state.contacts) { 
+      saveData(this.state.contacts);
+    };
+}
   // methods
   onFormSubmit = ({ name, number }) => {
     const contact = {
